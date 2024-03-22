7e21c42842f466d149b8beddb8dd081e797a83e9
ap0905 <149944573+ap0905@users.noreply.github.com>
2024-03-22 14:18:44 +0530

    Add files via upload

---
+++ chatbot/script.js
@@ -0,0 +1,15 @@
+window.botpressWebChat.init({
+    "composerPlaceholder": "Chat with bot",
+    "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
+    "botId": "2206fdd1-259f-4c19-a440-edbcc0c0f321",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "2206fdd1-259f-4c19-a440-edbcc0c0f321",
+    "webhookId": "df6a1e94-d563-4bf3-a210-d7f48d9a8789",
+    "lazySocket": true,
+    "themeName": "prism",
+    "frontendVersion": "v1",
+    "showPoweredBy": true,
+    "theme": "prism",
+    "themeColor": "#2563eb"
+});
